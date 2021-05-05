112595625b74d9a9de966920b1ecb09847469348
Shahul Alam <alam.shahul@gmail.com>
2021-05-05 00:22:45 -0400

    Added code for decompressing SHAP values embedding space to gene space.

--- explain.py
+++ explain.py
@@ -5,6 +5,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import shap
 
+def calculate_shap_values(model, training_data, test_data, K=None):
+    """Calculate SHAP values for the given model.
+
+    Args:
+        model: trained classification model
+        training_data: data of shape (num_samples, num_features) on which model was trained
+        test_data: data of shape (num_samples, num_features) on which model was tested 
+        K: number of means to use for K-means summarization of training_data (used for
+            calculating feature importances in SHAP values); if None, then all the
+            training data are used for this calculation (potentially expensive)
+    
+    Returns:
+        A tuple consisting of 1. the explainer's expected values and 2. the SHAP values.
+    """
+
+    if K:
+        kmeans_summary = shap.kmeans(training_data, K)
+        print("Finished k-means summarization!")
+        explainer = shap.KernelExplainer(svm.predict_proba, kmeans_summary, link="logit")
+    else:
+        explainer = shap.KernelExplainer(svm.predict_proba, training_data, link="logit")
+    
+    shap_values = explainer.shap_values(test_data, nsamples=100)
+
+    return explainer.expected_value, shap_values
+
+def decompress_shap_values_pca(shap_values, inverse_projection):
+    """Decompress PCA SHAP values in embedding space into gene-level equivalents.
+
+    Args:
+        shap_values: a matrix of SHAP values of shape
+            (num_classes, num_samples, num_principle_components)
+
+    Returns:
+        Decompressed, approximate SHAP values for all genes in the original dataset (i.e. a
+        matrix of shape (num_classes, num_samples, num_genes))
+    """
+
+    num_classes, num_samples, _ = shap_values.shape
+    _, num_genes = inverse_projection.shape
+    decompressed_shap_values = np.zeros((num_classes, num_samples, num_genes))
+    for class_index, class_shap_values in enumerate(shap_values):
+        decompressed_class_shap_values = class_shap_values @ inverse_projection
+        decompressed_shap_values[class_index] = decompressed_class_shap_values
+
+    return decompressed_shap_values
+
 if __name__ == "__main__":
     models_directory = Path("models")
 
@@ -18,15 +65,21 @@ if __name__ == "__main__":
 
     explanations_directory = Path("explanations")
     
-    K = 27
-    kmeans_summary = shap.kmeans(training_data, K)
-    print("Finished k-means summarization!")
-    explainer = shap.KernelExplainer(svm.predict_proba, kmeans_summary, link="logit")
+    # expected_value, shap_values = calculate_shap_values(svm.predict_proba, training_data, test_data, K=27)
+    # force_plot = shap.force_plot(expected_value[0], shap_values[0][0,:], test_data[0],# link="logit",
+    #         matplotlib=True, show=False)
+    # 
+    # np.save(explanations_directory / "forty_pc_svm_explainer_expected_values.npy", explainer.expected_value)
+    # np.save(explanations_directory / "forty_pc_svm_shap_values.npy", shap_values)
+    # force_plot.savefig(explanations_directory / "force_plot_first_example.png", dpi=150, bbox_inches='tight')
 
-    shap_values = explainer.shap_values(test_data, nsamples=100)
-    force_plot = shap.force_plot(explainer.expected_value[0], shap_values[0][0,:], test_data[0],# link="logit",
-            matplotlib=True, show=False)
-    
-    np.save(explanations_directory / "forty_pc_svm_explainer_expected_values.npy", explainer.expected_value)
-    np.save(explanations_directory / "forty_pc_svm_shap_values.npy", shap_values)
-    force_plot.savefig(explanations_directory / "force_plot_first_example.png", dpi=150, bbox_inches='tight')
+    shap_values = np.load(explanations_directory / "forty_pc_svm_shap_values.npy")
+    inverse_projection = np.load("forty_ptp_inv_pt.npy")
+
+    decompressed_shap_values = decompress_shap_values_pca(shap_values, inverse_projection.T)
+    classes = list(svm.classes_)
+    true_label_indices = [classes.index(label) for label in test_labels]
+    decompressed_true_class_shap_values = decompressed_shap_values[true_label_indices, range(len(test_labels))]
+    print(decompressed_true_class_shap_values.shape)
+    np.save(explanations_directory / "forty_pc_svm_decompressed_true_shap_values.npy", decompressed_true_class_shap_values)
+    # np.save(explanations_directory / "forty_pc_svm_decompressed_shap_values.npy", decompressed_shap_values)
